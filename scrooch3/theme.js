8318e770f65afc82c5aa79b078d2740540790a65
Mahigo-Repos <fossguru7@gmail.com>
2026-09-17 18:01:44 +0600

    Update theme.js

--- scrooch3/theme.js
+++ scrooch3/theme.js
@@ -1,6 +1,6 @@
 (function () {
   // Set your main color here
-  const navColor = 'purple'; // Background
+  const navColor = 'blue'; // Background
   
   // Darken a hex color by a percentage (0-1)
   function darkenColor(hex, amount = 0.25) {
